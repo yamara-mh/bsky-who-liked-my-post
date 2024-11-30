332adaa5e2e0e2edf48f6959f5eaeb244bb35332
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-11-30 19:37:53 +0900

    update

--- scripts/deleteFeedGenerator.ts
+++ scripts/deleteFeedGenerator.ts
@@ -20,22 +20,6 @@ const run = async () => {
   const agent = new AtpAgent({ service: 'https://bsky.social' })
   await agent.login({ identifier: handle, password })
 
-  // const did = agent.session?.did ?? ''
-
-  // const checkRecord = {
-  //   feed:'at://' + did + '/app.bsky.feed.generator/' + recordName
-  // }
-  // console.log(checkRecord);
-
-  // try {
-  //   await agent.api.app.bsky.feed.getFeedGenerator(checkRecord)
-  // } catch (err) {
-  //   throw new Error(
-  //     'The specified feed is not registered.',
-  //   )
-  // }
-  
-
   let record = {
     repo: agent.session?.did ?? '',
     collection: ids.AppBskyFeedGenerator,
