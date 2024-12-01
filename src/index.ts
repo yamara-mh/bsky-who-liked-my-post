0f2d13aa04c7733a718db26c19b7cde5d8c16f66
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-12-01 23:51:15 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ const run = async () => {
   const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
   const serviceDid =
     maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
+  
+  console.log(serviceDid)
+  console.log(process.env.FEEDGEN_PORT as string)
+  console.log(process.env.FEEDGEN_LISTENHOST as string)
+  console.log(process.env.FEEDGEN_SQLITE_LOCATION as string)
+  console.log(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT as string)
+  console.log(process.env.FEEDGEN_PUBLISHER_DID as string)
+  
   const server = FeedGenerator.create({
     port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
     listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
