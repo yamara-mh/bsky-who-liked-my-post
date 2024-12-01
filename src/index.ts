6cce41d0ac2cf6581289cc54706120fb8cf5a1c6
yamara <39893033+yamara-mh@users.noreply.github.com>
2024-12-02 03:10:33 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -3,30 +3,25 @@ import FeedGenerator from './server'
 
 const run = async () => {
   dotenv.config()
-  let hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
-  hostname += ".yamara.workers.dev"
+  // const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
+  const hostname += (process.env.FEEDGEN_HOSTNAME as string) + ".yamara.workers.dev"
 
-  const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
-  
-  console.log(serviceDid)
-  console.log(process.env.FEEDGEN_PORT as string)
-  console.log(process.env.FEEDGEN_LISTENHOST as string)
-  console.log(process.env.FEEDGEN_SQLITE_LOCATION as string)
-  console.log(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT as string)
-  console.log(process.env.FEEDGEN_PUBLISHER_DID as string)
+  const serviceDid = "did:plc:6hj3b43unydeqry6lqobe4b5"
+     // maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
   
   const server = FeedGenerator.create({
-    port: maybeInt(process.env.FEEDGEN_PORT) ?? 443,
-    listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? '0.0.0.0', // 'localhost',
-    sqliteLocation: maybeStr(process.env.FEEDGEN_SQLITE_LOCATION) ?? ':memory:',
-    subscriptionEndpoint:
-      maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ??
-      "did:plc:6hj3b43unydeqry6lqobe4b5",// 'wss://bsky.network',
-    publisherDid:
-      maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? "did:plc:6hj3b43unydeqry6lqobe4b5",// 'did:example:alice',
-    subscriptionReconnectDelay:
-      maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
+    port: 443,
+      // maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
+    listenhost: '0.0.0.0',
+      // maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
+    sqliteLocation: ':memory:',
+      // maybeStr(process.env.FEEDGEN_SQLITE_LOCATION) ?? ':memory:',
+    subscriptionEndpoint: 'wss://bsky.network',
+      // maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ?? 'wss://bsky.network',
+    publisherDid:"did:plc:6hj3b43unydeqry6lqobe4b5",
+      // maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? 'did:example:alice',
+    subscriptionReconnectDelay: 86400,
+      // maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
     hostname,
     serviceDid,
   })
